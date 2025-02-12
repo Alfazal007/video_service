82294006d27d8edfc6808f91245f37f9f5148d38
Alfazal007 <alfazal7777@gmail.com>
2025-02-12 22:29:55 +0530

    init actix web app

--- auth_service/src/main.rs
+++ auth_service/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_web::{middleware::Logger, web, App, HttpServer};
+
+pub mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenvy::dotenv().expect("Issue fetching the env variables");
+    let _database_url = env::var("DATABASE_URL").expect("Database url not provided in the env");
+
+    env_logger::Builder::new().parse_filters("info").init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(web::scope("/api/v1/user").route(
+                "/create",
+                web::post().to(crate::routes::user_controllers::create_user::create_user),
+            ))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
---
+++ auth_service/src/routes/user_controllers/create_user.rs
@@ -0,0 +1,5 @@
+use actix_web::Responder;
+
+pub async fn create_user() -> impl Responder {
+    "create user route"
+}
